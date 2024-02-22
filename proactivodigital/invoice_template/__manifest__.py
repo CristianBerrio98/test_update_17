69bd7f9a70d890988dbb12a6198e088371d58789
Cristian Berrio <cristian.berrio@proactivo.digital>
2024-02-22 09:53:56 -0500

    template invoice

---
+++ proactivodigital/invoice_template/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Invoice template',
+    'version': '17.0.21.12.050100',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/view.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
